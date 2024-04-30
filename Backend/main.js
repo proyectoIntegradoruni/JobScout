a20a8c8562891e1a4c179b9dcf6922f81e523677
Lina <rivas.lina@correounivalle.edu.co>
2024-04-30 15:10:07 -0500

    buscar por el parametro recibido

--- Backend/main.js
+++ Backend/main.js
@@ -6,5 +6,7 @@ async function getResultFromGoogle(query)
 {
     const browser = await chromium.launch();
     const page = await browser.newPage();
-    
+    await page.goto('https://www.buscadordeempleo.gov.co/#/home');
+    await page.type('input[name = "search-job-input"]',query)
+
 }
