e034a0ace966b0629b335031b3ddecfef0cdb921
Marcela <rivas.lina@correounivalle.edu.co>
2024-05-01 15:29:43 -0500

    verificacion de resultados en consola

--- Backend/main.js
+++ Backend/main.js
@@ -1,4 +1,4 @@
-import {chromium} from 'playwright';
+const { chromium } = require('playwright');
 
 // generar resultados
 
@@ -7,6 +7,31 @@ async function getResultFromGoogle(query)
     const browser = await chromium.launch();
     const page = await browser.newPage();
     await page.goto('https://www.buscadordeempleo.gov.co/#/home');
-    await page.type('input[name = "search-job-input"]',query)
+    await page.waitForSelector('input[name = "search-job-input"]')
+    await page.type('input[name = "search-job-input"]',query);
+    page.keyboard.press('Enter');
+    console.log('he llegado hasta enter')
+    await page.waitForSelector('div[class = "results-card-container"]')
+    //await page.waitForNavigation({waitUntil: 'networkidle'});
+    console.log('he llegado mas abajo')
+    const listadoResultados = await page.evaluate(()=> {
+        let resultados = [];
+        console.log('entramos a lista de resultado')
+        document.querySelectorAll('div[class = "results-card-container"] a').forEach((anchor, index)=>{
+            resultados.push({
+                index: index,
+                title: anchor.innerText,
+                url: anchor.href,
+            });
+        });
+        return resultados;
+    });
+
+    console.log(listadoResultados);
+    await browser.close();
+
 
 }
+
+
+getResultFromGoogle('nodejs');
