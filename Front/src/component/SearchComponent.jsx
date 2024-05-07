f06005e0de1f97090c8cec071f853705dee8f2cd
KPaola-M74 <kampao74@gmail.com>
2024-05-07 09:41:16 -0500

    actilizacion estilos

--- Front/src/component/SearchComponent.jsx
+++ Front/src/component/SearchComponent.jsx
@@ -11,20 +11,11 @@ const SearchComponent = () => {
   const [search2, setSearch2] = useState("");
   const [cards,setcards] = useState([
     {
-      id: 1,
-      title: "Card 1",
-      url: "https://www.google.com/webhp?authuser=1",
-    },
-    {
-      id: 2,
-      title: "Card 2",
-      image: "image2.jpg",
-      url: "https://example.com/card2",
-    },
-    {
+     
+    
       id: 3,
       title: "Card 3",
-      content : "cdshbcjd cgiysdgc uisdcgids cguis gch9su7 gc7lsicg7lsic gisyc g7syi cgisycgh isuc guisc hyscg sylic gslyic gslicysgcgs8c ngsujgc sycu gscl snbcgsl9 csfcs8cbs csciscgs8c igsvc7sliychbsgcysw",
+      content : "Somos un BPO y nos encontramos en busca de personal apasionado. Perfil Académico: Bachiller. Experiencia Laboral: Experiencia mínima 3 meses en servicio al cliente, Funciones: Brindar asesoría comercial y de servicio a los clientes, realizar proceso de retención de acuerdo a las metricas establecidas por la campana. Devolver oportunamente las llamadas a los clientes cuando se presente corte o caída antes de la finalización o cierre del contacto. Horario: : Lunes a viernes de 7:45 am a 7:30 pm - sábados de 8 am a 1pm 47 horas semanales DOMINGOS Y FESTIVOS NO SE LABORAN Salario: 1.300.000 + Comisiones 650.000 techo + incentivos de internos de aproximadamente $350.000 con prestaciones de Ley + 162.000 Aux transporte legal) con prestaciones de Ley. Sede: Carvajal- cerca al portal el dorado de TransMilenio Seminario/Capa: 15 días presenciales - todos los días son pagos a $15.000 Contrato: Obra labor",
      
       url: "https://example.com/card3",
     },
@@ -111,13 +102,14 @@ const SearchComponent = () => {
     <button  className="boton2" type="submit"    style={{ margin: '50px', padding: 0 }} onClick={handleBuscar}>Buscar</button>
 
     <div className="d-flex justify-content-center align-items-center h-100">
-      <Grid container spacing={5}>
-        {cards.map(({ title, content, url, id }) => (
-          <Grid item xs={12} sm={6} md={3} key={id}>
-            <Card  title={title}  content= {content}url={url} id={id} />
-          </Grid>
-        ))}
+    <Grid container spacing={3}>
+    {cards.map(({ title, content, url, id }) => (
+      <Grid item xs={12} sm={6} md={6} lg={6} key={id}> {/* Cambiado a 6 columnas para mostrar 2 en una fila */}
+        <Card title={title} content={content} url={url} id={id} />
       </Grid>
+    ))}
+  </Grid>
+
     </div>
     </div>
   );
--- Front/src/component/tarjeta.jsx
+++ Front/src/component/tarjeta.jsx
@@ -1,27 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 import "./card.css";
-function Card({  title,content,url, id }) {
+
+function Card({ title, content, url, id }) {
+  const [showInfo, setShowInfo] = useState(false);
+
   const handleClick = () => {
-    window.location.href = url;
+    window.open(url, '_blank');
+  };
+
+  const handleMouseOver = () => {
+    setShowInfo(true);
+  };
+
+  const handleMouseOut = () => {
+    setShowInfo(false);
   };
 
   return (
-    <div>
+    <div     >
       <div
         className="card text-center bg-dark animate__animated animate__fadeInUp"
         style={{
           backgroundColor: "#FFFFFF",
           border: "2px solid #FFFFFF",
           borderRadius: "30px",
+          width : "30rem"        
         }}
-        onClick={handleClick} // Added onClick handler
+        onClick={handleClick}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
       >
         <div className="overflow2">
-                  <div className="card-body text-light">
-            <h4 className="card-title">{title}</h4>
-            <h1 className="card-text">{content}</h1>
+          <div className="card-body text-light">
+          <p style={{
+            fontSize: "1.5rem",
+            fontFamily: "'Roboto Mono', sans-serif",
+            fontWeight: "bold"
+          }}>{title}</p>
+
+           
+            {showInfo && (
+              <div className="tooltip">
+                <p style={{
+            fontSize: "1.5rem",
+            fontFamily: "'Roboto Mono', monospace",
+         
+          }}>{content}</p>
+              </div>
+            )}
           </div>
         </div>
       </div>
@@ -34,7 +62,6 @@ Card.propTypes = {
   url: PropTypes.string,
   id: PropTypes.string,
   content: PropTypes.string,
- 
 };
 
 export default Card;
