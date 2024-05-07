7879afb6101947fbf3580eef8edb4c1ddf87dcb1
KPaola-M74 <kampao74@gmail.com>
2024-05-06 21:06:14 -0500

    log

--- Front/src/component/sidebar.jsx
+++ Front/src/component/sidebar.jsx
@@ -14,13 +14,21 @@ const Sidebar = ({children}) => {
   const menuItem = [
    
     {
-      path: "/camara",
-      name: "Reconocer Emociones",
+      path: "/Login",
+      name: "Iniciar sesion",
       icon:<BiVideoRecording />
+
     },
     {
-      path: "/history",
-      name: "Historial de emociones",
+      path: "/registro",
+      name: "Registrarse",
+      icon:<MdPeopleAlt />
+
+    }
+    ,
+    {
+      path: "/alerta",
+      name: "Alerta",
       icon:<MdPeopleAlt />
 
     }
@@ -48,13 +56,12 @@ const Sidebar = ({children}) => {
           }
       </div>
       <div style={{ width: isOpen ? "calc(100% - 300px)" : "calc(100% - 90px)" }} className="top_sectionp">
-    <h3 style={{ fontSize: '40px', color: '#88DFF2' }}>Bienvenidos a AEVA</h3>
-            <h3 className='Descripcion'>Analizador de Emociones Visuales </h3> 
-            <h3 className='Conten'>Con AEVA podras reconocer las emociones<br></br>
-             o expresiones faciales de los demás en <br></br>
-             tiempo real. </h3>
-             
-             <h3 className='Conten'>En la parte izquierda encontras el Historial de emociones que reconocemos y la camara para que practiques. </h3> </div> 
+    <h3 style={{ fontSize: '90px', color: '#0D0D0D' }}>No hay alertas disponibles</h3>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"  width="80" height="30">
+        <path fill="#7ED957" d="M224 512c35.3 0 64-28.7 64-64H160c0 35.4 28.7 64 64 64zm215.4-149.7c-19.3-20.8-55.5-52-55.5-154.3 0-77.7-54.5-139.9-127.9-155.2V32c0-17.7-14.3-32-32-32s-32 14.3-32 32v20.8C118.6 68.1 64.1 130.3 64.1 208c0 102.3-36.2 133.5-55.5 154.3-6 6.5-8.7 14.2-8.6 21.7 .1 16.4 13 32 32.1 32h383.8c19.1 0 32-15.6 32.1-32 .1-7.6-2.6-15.3-8.6-21.7z"/>
+        </svg>
+ 
+           </div> 
 
       <main>{children} </main>
     </div>
